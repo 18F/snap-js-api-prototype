8e7c8c1c3de855e3a0c809eab45cd1d2781bbad5
Alex Soble <alex.soble@gsa.gov>
2020-06-04 14:52:10 -0500

    First pass at translating ParseInputData to JS

---
+++ src/input_data/parse_input_data.js
@@ -0,0 +1,151 @@
+export class ParseInputs {
+    // Detects if input data is valid, returns error messages if not.
+    //
+    // Also, cleans up input data sent to API:
+    //
+    // * Converts strings to integers as needed
+    // * Sets defaults
+
+    constructor(inputs) {
+        this.inputs = inputs;
+        this.errors = [];
+        this.result = null;
+    }
+
+    parse() {
+        if (!inputs) {
+            this.errors.append('No input data received.');
+            return;
+        }
+
+        // Handle required integer fields (household size, income, assets):
+        const REQUIRED_NUMBER_INPUTS = [
+            'household_size',
+            'monthly_job_income',
+            'monthly_non_job_income',
+            'resources',
+        ];
+
+        const REQUIRED_BOOLEAN_INPUTS = [
+            'household_includes_elderly_or_disabled'
+        ];
+
+        const OPTIONAL_BOOLEAN_INPUTS = [
+            'use_emergency_allotment'
+        ];
+
+        const OPTIONAL_NUMBER_INPUTS = [
+            'dependent_care_costs',
+            'medical_expenses_for_elderly_or_disabled',
+            'court_ordered_child_support_payments',
+            'rent_or_mortgage',
+            'homeowners_insurance_and_taxes',
+            'utility_costs',
+        ];
+
+        const UTILITY_ALLOWANCE_INPUT = 'utility_allowance';
+    }
+
+    handle_required_integer_input(input_key) {
+        const input_value = this.inputs[input_key];
+
+        if (input_value === null || input_value === undefined) {
+            this.errors.push(`Missing required input: ${input_key}`);
+            return false;
+        }
+
+        const try_parse_int = parseInt(input_value);
+
+        if (isNaN(try_parse_int)) {
+            this.errors.push(`Value for ${input_key} is not a number.`);
+            return false;
+        }
+
+        this.inputs[input_key] = try_parse_int;
+        return true;
+    }
+
+    handle_required_bool_input(input_key) {
+        const input_value = this.inputs[input_key];
+
+        if (input_value === null || input_value === undefined) {
+            this.errors.push(`Missing required input: ${input_key}`);
+            return false;
+        }
+
+        if (typeof input_value === 'string') {
+            this.inputs[input_key] = (input_value === 'true');
+        }
+    }
+
+    handle_optional_bool_input(input_key) {
+        const input_value = this.inputs[input_key];
+
+        if ([true, false, null].includes(input_value)) {
+            return true
+        } else if (typeof input_value === 'string') {
+            this.inputs[input_key] = (input_value === 'true');
+        } else {
+            this.errors.push(`Unexpected value for ${input_key}`);
+        }
+    }
+
+    handle_utility_allowance_input(input_key) {
+        const input_value = this.inputs[input_key];
+
+        const UTILITY_ALLOWANCES = [
+            'HEATING_AND_COOLING',
+            'BASIC_LIMITED_ALLOWANCE',
+            'SINGLE_UTILITY_ALLOWANCE',
+            'ELECTRICITY',
+            'GAS_AND_FUEL',
+            'PHONE',
+            'SEWAGE',
+            'TRASH',
+            'WATER',
+            // Clients can either explicitly specify that no SUA
+            // applies with 'NONE', or leave this optional field
+            // blank to the same effect.
+            'NONE',
+        ];
+
+        // Utility allowance can be blank or '', if the household is in a state
+        // that uses raw utility costs instead of standard utility allowances.
+        if (input_value === null || input_value === '') {
+            this.inputs[input_key] = null;
+            return true;
+        }
+
+        if (UTILITY_ALLOWANCES.includes(input_value)) {
+            return true;
+        } else {
+            this.errors.push(`Unknown standard utility allowance: ${input_value}`);
+            return false;
+        }
+    }
+
+    handle_optional_integer_input(input_key) {
+        // Check if the key exists in the inputs object
+        if (!(input_key in this.inputs)) {
+            this.inputs['input_key'] = 0;
+            return true;
+        }
+
+        const input_value = this.inputs[input_key];
+
+        if (typeof input_value === 'number') {
+            return true;
+        }
+
+        const try_parse_int = parseInt(input_value);
+
+        if (isNaN(try_parse_int)) {
+            this.errors.push(`Value for ${input_key} is not a number.`);
+            return false;
+        }
+
+        this.inputs[input_key] = try_parse_int;
+        return true;
+    }
+}
+
