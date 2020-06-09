d0986c4ecb01e1fceb8fdd966b4e0d91fbe21c67
Alex Soble <alex.soble@gsa.gov>
2020-06-09 14:44:57 -0400

    Add assertions to parse_inputs_test.js

--- test/parse_inputs_test.js
+++ test/parse_inputs_test.js
@@ -48,5 +48,50 @@ describe('ParseInputs', () => {
             'utility_costs': 0,
         });
     });
+
+
+    it('should parse string `true` to a boolean for a boolean field', () => {
+        const inputs = {
+            'state_or_territory': 'IL',
+            'monthly_job_income': '0',
+            'monthly_non_job_income': '0',
+            'household_size': '1',
+            'household_includes_elderly_or_disabled': 'true',
+            'resources': '0'
+        };
+
+        const parser = new ParseInputs(inputs);
+        parser.parse();
+
+        assert.equal(parser.errors.length, 0);
+        assert.deepEqual(parser.inputs, {
+            'court_ordered_child_support_payments': 0,
+            'dependent_care_costs': 0,
+            'homeowners_insurance_and_taxes': 0,
+            'household_includes_elderly_or_disabled': true,
+            'household_size': 1,
+            'medical_expenses_for_elderly_or_disabled': 0,
+            'monthly_job_income': 0,
+            'monthly_non_job_income': 0,
+            'rent_or_mortgage': 0,
+            'resources': 0,
+            'state_or_territory': 'IL',
+            'utility_costs': 0,
+        });
+    });
+
+
+    it('should throw an error if a required integer is missing', () => {
+        const inputs = {
+            'state_or_territory': 'IL',
+            'monthly_non_job_income': '0',
+            'household_size': '1',
+            'household_includes_elderly_or_disabled': 'true',
+            'resources': '0'
+        };
+
+        const parser = new ParseInputs(inputs);
+        assert.throws(parser.parse(), 'Error thrown must have this msg');
+    });
 });
 
